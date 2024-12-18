eb1b0c4aaa77d457efb6b6ea55c12871714148ed
Hera <mostafizhera@gmail.com>
2024-12-19 00:52:05 +0600

    added the jwt in job portal two project

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('dotenv').config()
 
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -9,7 +9,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eywn0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,7 +30,7 @@ async function run() {
 
         // jobs related apis
         const jobsCollection = client.db('jobPortal').collection('jobs');
-        const jobApplicationCollection = client.db('jobPortal').collection('job_applications');
+        // const jobApplicationCollection = client.db('jobPortal').collection('job_applications');
 
         // jobs related APIs
         app.get('/jobs', async (req, res) => {
